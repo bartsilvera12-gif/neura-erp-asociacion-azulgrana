bartsilvera12-gif/neura-erp-distribuidorajm

Sacar "Ayuda en linea" de la barra superior
Es el asistente de soporte del ERP original, no de Distribuidora JM. Sale del
header de desktop y del de mobile, junto con los imports que quedaban sin uso.

La pantalla /ayuda sigue existiendo: solo se saca el acceso de la barra.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_012wRbuUAjeL4csSb9GLGPEF

src/mobile/layout/MobileHeader.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
-import { Bell, HelpCircle, LogOut } from "lucide-react";
+import { Bell, LogOut } from "lucide-react";
 import { signOut } from "@/lib/auth";
 import { useUsuarioActual } from "@/shared/hooks/useUsuarioActual";
 
@@ -55,13 +54,6 @@ export default function MobileHeader() {
       </h1>
 
       <div className="flex items-center gap-1">
-        <Link
-          href="/ayuda"
-          aria-label="Ayuda en línea"
-          className="flex h-11 w-11 items-center justify-center rounded-lg text-[#475569] transition-colors hover:bg-slate-50"
-        >
-          <HelpCircle className="h-5 w-5" />
-        </Link>
         <button
           type="button"
           aria-label="Notificaciones"

bartsilvera12-gif/neura-erp-asociacion-azulgrana

src/mobile/layout/MobileHeader.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import Link from "next/link";
-import { Bell, HelpCircle, Menu } from "lucide-react";
+import { Bell, Menu } from "lucide-react";
 import { useUsuarioActual } from "@/shared/hooks/useUsuarioActual";
 
 /**
@@ -32,13 +31,6 @@ export default function MobileHeader({ onOpenMenu }: { onOpenMenu: () => void })
       <h1 className="text-sm font-semibold tracking-tight text-[#0F172A]">ZENTRA</h1>
 
       <div className="flex items-center gap-1">
-        <Link
-          href="/ayuda"
-          aria-label="Ayuda en línea"
-          className="flex h-11 w-11 items-center justify-center rounded-lg text-[#475569] transition-colors hover:bg-slate-50 hover:text-[#0EA5E9]"
-        >
-          <HelpCircle className="h-5 w-5" />
-        </Link>
         <button
           type="button"
           aria-label="Notificaciones"